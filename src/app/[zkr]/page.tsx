86ce46e02694e7c5f56c38efb68ee8a932d75eda
Ahmed Gabr <amoustafa@v4tech.com>
2024-05-18 16:10:53 +0300

    fix conflict

--- src/app/[zkr]/page.tsx
+++ src/app/[zkr]/page.tsx
@@ -1,30 +1,36 @@
+import React from "react";
 import SingleZkr from "@/components/cards/SingleZkr";
 import { getSingleZkr } from "@/services/api";
 import { zkrListItem } from "@/types";
-import React from "react";
 
-const page = async ({
-  searchParams,
-  params
-}: {
+interface PageProps {
   searchParams?: {
     id?: string;
-  },
-  params:{zkr:string}
-  
-}) => {
-  const title: string = decodeURIComponent(params?.zkr)
-  const data =  await getSingleZkr('ar', searchParams?.id) || []
-  return <div>
-    <h2 className="font-bold text-sky-900 text-2xl">{title}</h2>
-    <div className="">
-      {
-        data[title]?.map((item: zkrListItem) => <>
-          <SingleZkr {...{ text: item.ARABIC_TEXT, count:item.REPEAT, audio: item.AUDIO}} />
-        </>)
-      }
+  };
+  params: {
+    zkr: string;
+  };
+}
+
+const Page: React.FC<PageProps> = async ({ searchParams, params }) => {
+  const title: any = decodeURIComponent(params.zkr);
+  const data: any = (await getSingleZkr("ar", searchParams?.id)) || [];
+
+  return (
+    <div>
+      <h2 className="font-bold text-sky-900 text-2xl">{title}</h2>
+      <div>
+        {data[title]?.map((item: zkrListItem, index: number) => (
+          <SingleZkr
+            key={index}
+            text={item.ARABIC_TEXT}
+            count={item.REPEAT}
+            audio={item.AUDIO}
+          />
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
-export default page;
+export default Page;
